38013b229348db66857136b300500b5a5d1ef5e8
Miguel Soranno <msoranno@gmail.com>
2017-04-29 10:39:08 +0200

    Otro script

---
+++ gitInitialize.sh
@@ -0,0 +1,6 @@
+git config --global user.name "Miguel Soranno"
+git config --global user.email "msoranno@gmail.com"
+git config --global color.status auto
+git config --global color.branch auto
+git config --global push.default simple
+
